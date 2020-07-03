23c8b2a2bb2cac2f44e92eae071a118d240cfa16
Geert van der Cruijsen <geert@vdcruijsen.net>
2020-07-03 20:31:32 +0200

    fixed pihole working

--- pihole/helm.sh
+++ pihole/helm.sh
@@ -7,9 +7,4 @@
 #!/bin/bash
 set -ex
 
-helm repo add mojo2600 https://mojo2600.github.io/pihole-kubernetes/
-helm repo update
-
-helm upgrade --install --debug pihole-volumes ../pihole --namespace pihole
-
-helm upgrade --install pihole -f pihole.values.yml mojo2600/pihole --namespace=pihole
+helm upgrade --install --debug pihole ../pihole --namespace pihole
